b09e04016cc5a71fd45d35e89c7d7e27e7f26c74
ophub <ophub2020@gmail.com>
2022-01-24 23:03:04 +0800

    update kernel compilation

--- compile-kernel/tools/script/generate_uinitrd.sh
+++ compile-kernel/tools/script/generate_uinitrd.sh
@@ -43,24 +43,19 @@ chroot_generate_uinitrd() {
     echo -e "${STEPS} Generate uInitrd file."
     #echo -e "${INFO} File status in the /boot directory before the update: \n$(ls -l .) \n"
 
-    # Backup the original uInitrd file
-    [ -f uInitrd ] && mv -f uInitrd uInitrd.bak 2>/dev/null
-    [ ! -f zImage ] && cp -f vmlinuz-${chroot_kernel_version} zImage 2>/dev/null && sync
+    cp -f vmlinuz-${chroot_kernel_version} zImage 2>/dev/null && sync
 
     # Generate uInitrd file directly under armbian system
     update-initramfs -c -k ${chroot_kernel_version} 2>/dev/null
 
     if [ -f uInitrd ]; then
-        echo -e "${SUCCESS} The uInitrd file is Successfully generated."
+        echo -e "${SUCCESS} The initrd.img and uInitrd file is Successfully generated."
         mv -f uInitrd uInitrd-${chroot_kernel_version} 2>/dev/null && sync
     else
-        echo -e "${WARNING} The uInitrd file not updated."
+        echo -e "${WARNING} The initrd.img and uInitrd file not updated."
     fi
 
     echo -e "${INFO} File situation in the /boot directory after update: \n$(ls -l *${chroot_kernel_version}) \n"
-
-    # Restore the original uInitrd
-    [ -f uInitrd.bak ] && mv -f uInitrd.bak uInitrd 2>/dev/null
 }
 
 #chroot_env_init
