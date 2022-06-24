ed40ccd16852ebe2ea43da45c16270f306c7e6e0
ophub <ophub2020@gmail.com>
2022-06-24 17:47:19 +0800

    armbian-software: change storage path

---
+++ build-armbian/common-files/rootfs/usr/share/ophub/armbian-software/301-frps.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+#============================================================================
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of the Armbian for Amlogic TV Boxes
+# https://github.com/ophub/amlogic-s9xxx-armbian
+#
+# Function: Execute software install/update/remove script
+# Copyright (C) 2021- https://github.com/unifreq/openwrt_packit
+# Copyright (C) 2021- https://github.com/ophub/amlogic-s9xxx-armbian
+#
+# Command: ${0} <install/update/remove>
+# Example: ${0} install
+#          ${0} update
+#          ${0} remove
+#
+#============================== Functions list ==============================
+#
+# error_msg           : Output error message
+# software_download   : Software download
+# software_install    : Software install
+# software_update     : Software update
+# software_remove     : Software remove
+#
+#========================== Set default parameters ==========================
+#
+# Define the software
+software_name="frps"
+software_core="/usr/bin/frps"
+software_conf="/etc/frp/frps.ini"
+software_service="/etc/systemd/system/frps.service"
+#
+# Set font color
+STEPS="[\033[95m STEPS \033[0m]"
+INFO="[\033[94m INFO \033[0m]"
+SUCCESS="[\033[92m SUCCESS \033[0m]"
+PROMPT="[\033[93m PROMPT \033[0m]"
+ERROR="[\033[91m ERROR \033[0m]"
+#
+#============================================================================
+
+# Show error message
+error_msg() {
+    echo -e "${ERROR} ${1}"
+    exit 1
+}
+
+software_download() {
+    echo -e "${STEPS} Start downloading [ ${software_name} ] software..."
+
+    # Software version query api
+    software_api="https://api.github.com/repos/fatedier/frp/releases"
+    # Check the latest version, E.g: v0.43.0
+    software_latest_version="$(curl -s "${software_api}" | grep "tag_name" | awk -F '"' '{print $4}' | tr " " "\n" | sort -rV | head -n 1)"
+    # Query download address, E.g: https://github.com/fatedier/frp/releases/download/v0.43.0/frp_0.43.0_linux_arm64.tar.gz
+    software_url="$(curl -s "${software_api}" | grep -oE "https:.*${software_latest_version}.*linux_arm64.*.tar.gz")"
+    echo -e "${INFO} Software download from: [ ${software_url} ]"
+
+    # Download software, E.g: /tmp/tmp.xxx/frp_0.43.0_linux_arm64.tar.gz
+    tmp_download="$(mktemp -d)"
+    software_filename="${software_url##*/}"
+    wget -q -P ${tmp_download} ${software_url}
+    [[ "${?}" -eq "0" && -s "${tmp_download}/${software_filename}" ]] || error_msg "Software download failed!"
+    echo -e "${INFO} Software downloaded successfully: $(ls ${tmp_download} -l)"
+
+    # Unzip the download file
+    tar -xf ${tmp_download}/${software_filename} -C ${tmp_download} && sync
+    # software directory, E.g: /tmp/tmp.xxx/frp_0.43.0_linux_arm64
+    software_dir="${tmp_download}/${software_filename/.tar.gz/}"
+    echo -e "${INFO} Software directory: [ ${software_dir} ]"
+}
+
+software_install() {
+    echo -e "${STEPS} Start installing [ ${software_name} ] software..."
+    software_download
+
+    # Generate random token
+    random_token="$(cat /proc/sys/kernel/random/uuid)"
+    # Generate random account
+    random_account="$(cat /proc/sys/kernel/random/uuid)"
+    random_user="${random_account:0:18}"
+    random_pwd="${random_account:19}"
+
+    # Add frps.ini
+    echo -e "${INFO} Add frps.ini to: [ ${software_conf} ]"
+    [[ -d "/etc/frp" ]] || sudo mkdir -p "/etc/frp"
+    #sudo cp -f ${software_dir}/frps.ini /etc/frp
+    sudo cat >${software_conf} <<EOF
+[common]
+bind_addr = 0.0.0.0
+bind_port = 7000
+bind_udp_port = 7001
+kcp_bind_port = 7000
+vhost_http_port = 80
+vhost_https_port = 443
+dashboard_addr = 0.0.0.0
+dashboard_port = 7500
+dashboard_user = ${random_user}
+dashboard_pwd = ${random_pwd}
+log_file = /tmp/frps.log
+log_level = info
+log_max_days = 1
+token = ${random_token}
+subdomain_host = frps.com
+max_pool_count = 5
+tcp_mux = true
+
+EOF
+
+    # Copy frps
+    echo -e "${INFO} Copy frps to: [ ${software_core} ]"
+    sudo cp -f ${software_dir}/frps /usr/bin && chmod +x ${software_core}
+
+    # Add frps.service
+    echo -e "${INFO} Add frps.service to: [ ${software_service} ]"
+    sudo cat >${software_service} <<EOF
+[Unit]
+Description=frps
+After=network.target
+
+[Service]
+Type=simple
+User=nobody
+Restart=on-failure
+RestartSec=5s
+ExecStart=${software_core} -c ${software_conf}
+LimitNOFILE=1048576
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+    sync
+
+    # Set up to start automatically
+    echo -e "${INFO} Start service..."
+    sudo systemctl daemon-reload
+    sudo systemctl start frps
+    sudo systemctl enable frps
+
+    echo -e "${PROMPT} Instructions for use:"
+    cat <<EOF
+============================================[ Instructions for use of frps ]============================================
+Official website: https://github.com/fatedier/frp
+Documentation: https://github.com/ophub/amlogic-s9xxx-armbian/blob/main/build-armbian/armbian-docs/armbian_software.md
+
+Core file path: [ ${software_core} ]
+Config file path: [ ${software_conf} ]
+Service file path: [ ${software_service} ]
+
+Operate:
+        1. Edit config file: [ vi ${software_conf} ]
+        2. restart the service: [ sudo systemctl restart frps ]
+        3. Check running status: [ sudo systemctl status frps ]
+
+Service management commands:
+        sudo systemctl start frps
+        sudo systemctl stop frps
+        sudo systemctl restart frps
+        sudo systemctl enable frps
+        sudo systemctl status frps
+========================================================================================================================
+
+EOF
+
+    echo -e "${SUCCESS} Software installed successfully."
+    exit 0
+}
+
+software_update() {
+    echo -e "${STEPS} Start updating [ ${software_name} ] software..."
+    software_download
+
+    # Stop frps.service
+    sudo systemctl stop frps
+    sync && sleep 3
+
+    # Copy frps
+    echo -e "${INFO} Copy frps to: [ ${software_core} ]"
+    sudo cp -f ${software_dir}/frps /usr/bin && chmod +x ${software_core}
+    sync
+
+    # Set up to start automatically
+    sudo systemctl daemon-reload
+    sudo systemctl start frps
+    sudo systemctl enable frps
+
+    echo -e "${SUCCESS} Software update successfully."
+    exit 0
+}
+
+software_remove() {
+    echo -e "${STEPS} Start removing [ ${software_name} ] software..."
+
+    # Stop frps.service
+    sudo systemctl stop frps
+    sync && sleep 3
+
+    # Remove frps and frps.service
+    sudo rm -rf /etc/frp
+    sudo rm -f ${software_core}
+    sudo rm -f ${software_service} 2>/dev/null
+    sudo systemctl daemon-reload
+
+    echo -e "${SUCCESS} Software removed successfully."
+    exit 0
+}
+
+software_help() {
+    echo -e "${STEPS} Script usage instructions."
+
+    cat <<EOF
+=====================================================================
+ Function         : [ Command ]
+---------------------------------------------------------------------
+ Install Software : [ ${0} install ]
+ Update  Software : [ ${0} update  ]
+ Remove  Software : [ ${0} remove  ]
+=====================================================================
+EOF
+    exit 0
+}
+
+# Check script permission, supports running on Armbian system.
+echo -e "${STEPS} Welcome to [ ${software_name} ] software script: [ ${0} ]"
+[[ "$(id -u)" == "0" ]] || error_msg "Please run this script as root: [ sudo ${0} ]"
+#
+# Execute script assist functions
+case "${1}" in
+    install) software_install ;;
+    update)  software_update ;;
+    remove)  software_remove ;;
+    *)       software_help ;;
+esac
+
+exit 0
---
+++ build-armbian/common-files/rootfs/usr/share/ophub/armbian-software/software-command.sh
@@ -0,0 +1,413 @@
+#!/bin/bash
+#============================================================================
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of the Armbian for Amlogic TV Boxes
+# https://github.com/ophub/amlogic-s9xxx-armbian
+#
+# Function: Execute software install/update/remove command
+# Copyright (C) 2021- https://github.com/unifreq/openwrt_packit
+# Copyright (C) 2021- https://github.com/ophub/amlogic-s9xxx-armbian
+#
+# Command: software-command -s <software_id> -m <install/update/remove>
+# Example: software-command -s 101 -m install
+#          software-command -s 101 -m update
+#          software-command -s 101 -m remove
+#
+#============================== Functions list ==============================
+#
+# error_msg         : Output error message
+# check_release     : Check release file
+# software_install  : Install package
+# software_update   : Update package
+# software_remove   : Remove package
+# init_var          : Initialize variables
+#
+# software_101      : For docker
+# software_102      : For portainer(docker)
+# software_103      : For transmission(docker)
+#
+# software_201      : For desktop
+# software_202      : For vlc-media-player(desktop)
+# software_203      : For firefox(desktop)
+#
+#========================== Set default parameters ==========================
+#
+# Get custom firmware information
+ophub_release_file="/etc/ophub-release"
+#
+# Set font color
+STEPS="[\033[95m STEPS \033[0m]"
+INFO="[\033[94m INFO \033[0m]"
+SUCCESS="[\033[92m SUCCESS \033[0m]"
+OPTIONS="[\033[93m OPTIONS \033[0m]"
+NOTE="[\033[93m NOTE \033[0m]"
+ERROR="[\033[91m ERROR \033[0m]"
+#
+#============================================================================
+
+# Show error message
+error_msg() {
+    echo -e "${ERROR} ${1}"
+    exit 1
+}
+
+check_release() {
+    if [[ -f "${ophub_release_file}" ]]; then
+        source "${ophub_release_file}" 2>/dev/null
+        VERSION_CODEID="${VERSION_CODEID}"
+    else
+        error_msg "${ophub_release_file} file is missing!"
+    fi
+
+    [[ -n "${VERSION_CODEID}" ]] || error_msg "${ophub_release_file} value is missing!"
+}
+
+software_install() {
+    install_list="${1}"
+    echo -e "${STEPS} Start installing packages: [ ${install_list} ]..."
+
+    # Install the package
+    sudo apt-get update
+    [[ -n "${install_list}" ]] && sudo apt-get install -y ${install_list}
+
+    echo -e "${SUCCESS} [ ${install_list} ] packages installed successfully."
+}
+
+software_update() {
+    echo -e "${STEPS} Start updating packages..."
+
+    # Update the package
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get dist-upgrade -y
+    sudo apt-get --purge autoremove -y
+    sudo apt-get autoclean -y
+
+    echo -e "${SUCCESS} Package updated successfully."
+    exit 0
+}
+
+software_remove() {
+    remove_list="${1}"
+    echo -e "${STEPS} Start removing packages: [ ${remove_list} ]..."
+
+    # Update the package
+    sudo apt-get update
+    [[ -n "${remove_list}" ]] && sudo apt-get remove -y ${remove_list}
+    sudo apt-get --purge autoremove -y
+    sudo apt-get autoclean -y
+
+    echo -e "${SUCCESS} [ ${remove_list} ] packages removed successfully."
+}
+
+# For docker
+software_101() {
+    echo -e "${STEPS} Start executing the command..."
+    echo -e "${INFO} Software Name: [ docker ]"
+    echo -e "${INFO} Software ID: [ ${software_id} ]"
+    echo -e "${INFO} Software Manage: [ ${software_manage} ]"
+
+    case "${software_manage}" in
+    install)
+        armbian-docker install
+        ;;
+    update)
+        armbian-docker update
+        ;;
+    remove)
+        armbian-docker remove
+        ;;
+    *)
+        error_msg "Invalid input parameter: [ ${@} ]"
+        ;;
+    esac
+}
+
+# For portainer
+software_102() {
+    echo -e "${STEPS} Start executing the command..."
+    echo -e "${INFO} Software Name: [ portainer ]"
+    echo -e "${INFO} Software ID: [ ${software_id} ]"
+    echo -e "${INFO} Software Manage: [ ${software_manage} ]"
+
+    case "${software_manage}" in
+    install)
+        echo -ne "${OPTIONS} Select Install Portainer-ce?  No=(n) / LAN ip access=(h) / Domain cert access=(s): "
+        read pt
+        case "${pt}" in
+        h | H | http)
+            soft_opt="portainer_lan"
+            ;;
+        s | S | https)
+            soft_opt="portainer_domain"
+            ;;
+        *)
+            echo -e "${INFO} Finish the installation." && exit 0
+            ;;
+        esac
+        armbian-docker ${soft_opt}
+        ;;
+    update)
+        armbian-docker update
+        ;;
+    remove)
+        armbian-docker portainer_remove
+        ;;
+    *)
+        error_msg "Invalid input parameter: [ ${@} ]"
+        ;;
+    esac
+}
+
+# For transmission
+software_103() {
+    echo -e "${STEPS} Start executing the command..."
+    echo -e "${INFO} Software Name: [ transmission ]"
+    echo -e "${INFO} Software ID: [ ${software_id} ]"
+    echo -e "${INFO} Software Manage: [ ${software_manage} ]"
+
+    # Generate random account
+    random_account="$(cat /proc/sys/kernel/random/uuid)"
+    # transmission installation path
+    tr_path="/opt/docker/transmission"
+    tr_default_user="admin"
+    tr_default_pass="${random_account:0:18}"
+
+    case "${software_manage}" in
+    install)
+        echo -e "${STEPS} Start installing the docker image: [ transmission ]..."
+
+        # Check script permission
+        [[ "$(id -u)" == "0" ]] || error_msg "please run this script as root: [ sudo ${0} -s 103 -m install ]"
+
+        echo -ne "${OPTIONS} Set login username, the default is [ ${tr_default_user} ]: "
+        read tr_user
+        [[ -z "${tr_user}" ]] && tr_user="${tr_default_user}"
+        echo -e "${INFO} Login username: [ ${tr_user} ]"
+
+        echo -ne "${OPTIONS} Set login password, the default is [ ${tr_default_pass} ]: "
+        read tr_pass
+        [[ -z "${tr_pass}" ]] && tr_pass="${tr_default_pass}"
+        echo -e "${INFO} Login password: [ ${tr_pass} ]"
+
+        # Instructions: https://github.com/linuxserver/docker-transmission
+        echo -e "${STEPS} Start pulling the docker image: [ linuxserver/transmission:arm64v8-latest ]..."
+        docker run -d --name=transmission \
+            -e PUID=1000 \
+            -e PGID=1000 \
+            -e TZ=Asia/Shanghai \
+            -e TRANSMISSION_WEB_HOME=/transmission-web-control/ \
+            -e USER=${tr_user} \
+            -e PASS=${tr_pass} \
+            -p 9091:9091 \
+            -p 51413:51413 \
+            -p 51413:51413/udp \
+            -v ${tr_path}/config:/config \
+            -v ${tr_path}/downloads:/downloads \
+            -v ${tr_path}/watch/folder:/watch \
+            --restart unless-stopped \
+            linuxserver/transmission:arm64v8-latest
+
+        # Set the transmission-web-control
+        echo -e "${STEPS} Start the installation interface: [ transmission-web-control ]..."
+        tr_cn_url="https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh"
+        bash <(curl -fsSL ${tr_cn_url}) ${tr_path}
+
+        sync && sleep 3
+        echo -e "${NOTE} The transmission address [ http://ip:9091 ]"
+        echo -e "${NOTE} The transmission account [ username:${tr_user}  /  password:${tr_pass} ]"
+        echo -e "${SUCCESS} Transmission installed successfully."
+        exit 0
+        ;;
+    update)
+        # update transmission docker image
+        echo -e "${STEPS} Start updating the transmission docker image..."
+        docker pull linuxserver/transmission:arm64v8-latest
+
+        # Set the transmission-web-control
+        echo -e "${STEPS} Start updating the interface: [ transmission-web-control ]..."
+        tr_cn_url="https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh"
+        bash <(curl -fsSL ${tr_cn_url}) ${tr_path}
+
+        # Set the transmission-web-control
+        echo -e "${STEPS} Restart the transmission docker container..."
+        docker restart $(docker ps -aq --filter name=transmission)
+        ;;
+    remove)
+        # Query the container ID based on the image name and delete it
+        echo -e "${INFO} Start removing transmission container..."
+        docker stop $(docker ps -aq --filter name=transmission)
+        docker rm $(docker ps -aq --filter name=transmission)
+
+        # Query the image ID based on the image name and delete it
+        echo -e "${INFO} Start removing transmission image..."
+        docker image rm $(docker images -q --filter reference=linuxserver/transmission*:*)
+
+        # Delete the Transmission installation directory
+        [[ -d "${tr_path}" ]] && rm -rf ${tr_path}
+
+        echo -e "${SUCCESS} Transmission removed successfully."
+        exit 0
+        ;;
+    *)
+        error_msg "Invalid input parameter: [ ${@} ]"
+        ;;
+    esac
+}
+
+# For desktop
+software_201() {
+    echo -e "${STEPS} Start executing the command..."
+    echo -e "${INFO} Software Name: [ desktop ]"
+    echo -e "${INFO} Software ID: [ ${software_id} ]"
+    echo -e "${INFO} Software Manage: [ ${software_manage} ]"
+
+    case "${software_manage}" in
+    install)
+        if [[ "${VERSION_CODEID}" == "ubuntu" ]]; then
+            # Install ubuntu-desktop(gdm3) on Ubuntu (jammy/focal)
+            software_install "ubuntu-desktop lightdm"
+
+            sync && sleep 3
+            echo -e "${SUCCESS} Desktop installation is successful, restarting..."
+            reboot
+        elif [[ "${VERSION_CODEID}" == "debian" ]]; then
+            # Install Xfce(lightdm) on Debian 11 (bullseye)
+            software_install "task-xfce-desktop lightdm"
+
+            sync && sleep 3
+            echo -e "${SUCCESS} Desktop installation is successful, restarting..."
+            reboot
+        else
+            error_msg "VERSION_CODEID not supported: [ ${VERSION_CODEID} ]"
+        fi
+        ;;
+    remove)
+        if [[ "${VERSION_CODEID}" == "ubuntu" ]]; then
+            # Remove ubuntu-desktop(gdm3) on Ubuntu (jammy/focal)
+            software_remove "ubuntu-desktop lightdm"
+
+            sync && sleep 3
+            echo -e "${SUCCESS} Desktop removed successfully, restarting..."
+            reboot
+        elif [[ "${VERSION_CODEID}" == "debian" ]]; then
+            # Remove Xfce(lightdm) on Debian 11 (bullseye)
+            software_remove "task-xfce-desktop lightdm"
+
+            sync && sleep 3
+            echo -e "${SUCCESS} Desktop removed successfully, restarting..."
+            reboot
+        else
+            error_msg "VERSION_CODEID not supported: [ ${VERSION_CODEID} ]"
+        fi
+        ;;
+    update)
+        software_update
+        ;;
+    *)
+        error_msg "Invalid input parameter: [ ${@} ]"
+        ;;
+    esac
+}
+
+# For vlc-media-player
+software_202() {
+    echo -e "${STEPS} Start executing the command..."
+    echo -e "${INFO} Software Name: [ vlc-media-player ]"
+    echo -e "${INFO} Software ID: [ ${software_id} ]"
+    echo -e "${INFO} Software Manage: [ ${software_manage} ]"
+
+    case "${software_manage}" in
+    install)
+        software_install "vlc"
+        ;;
+    remove)
+        software_remove "vlc"
+        ;;
+    update)
+        software_update
+        ;;
+    *)
+        error_msg "Invalid input parameter: [ ${@} ]"
+        ;;
+    esac
+}
+
+# For firefox
+software_203() {
+    echo -e "${STEPS} Start executing the command..."
+    echo -e "${INFO} Software Name: [ firefox ]"
+    echo -e "${INFO} Software ID: [ ${software_id} ]"
+    echo -e "${INFO} Software Manage: [ ${software_manage} ]"
+
+    case "${software_manage}" in
+    install)
+        [[ "${VERSION_CODEID}" == "ubuntu" ]] && software_install "firefox"
+        [[ "${VERSION_CODEID}" == "debian" ]] && software_install "firefox-esr"
+        ;;
+    remove)
+        [[ "${VERSION_CODEID}" == "ubuntu" ]] && software_remove "firefox"
+        [[ "${VERSION_CODEID}" == "debian" ]] && software_remove "firefox-esr"
+        ;;
+    update)
+        software_update
+        ;;
+    *)
+        error_msg "Invalid input parameter: [ ${@} ]"
+        ;;
+    esac
+}
+
+# Initialize variables
+init_var() {
+    # If it is followed by [ : ], it means that the option requires a parameter value
+    get_all_ver="$(getopt "s:m:" "${@}")"
+
+    # Check the input parameters
+    while [[ -n "${1}" ]]; do
+        case "${1}" in
+        -s | --SoftwareID)
+            if [[ -n "${2}" ]]; then
+                software_id="${2}"
+                shift
+            else
+                error_msg "Invalid -s parameter [ ${2} ]!"
+            fi
+            ;;
+        -m | --Manage)
+            if [[ -n "${2}" ]]; then
+                if [[ "${2}" == "install" || "${2}" == "update" || "${2}" == "remove" ]]; then
+                    software_manage="${2}"
+                else
+                    error_msg "Invalid -m parameter [ ${2} ]!"
+                fi
+                shift
+            else
+                error_msg "Invalid -m parameter [ ${2} ]!"
+            fi
+            ;;
+        *)
+            error_msg "Invalid option [ ${1} ]!"
+            ;;
+        esac
+        shift
+    done
+
+    # Get related variables
+    check_release
+    # Execute the corresponding operation command
+    software_${software_id} ${software_manage}
+}
+
+# Check script permission, supports running on Armbian system.
+echo -e "${STEPS} Welcome to the software service command center: [ ${0} ]"
+[[ "$(id -u)" == "0" ]] || error_msg "Please run this script as root: [ sudo ${0} ]"
+#
+# Initialize variables
+init_var "${@}"
+
+exit 0
