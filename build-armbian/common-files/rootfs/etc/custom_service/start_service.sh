d637701b68e0662cef2fb2e1832aa9137e4793d7
ophub <ophub2020@gmail.com>
2022-10-13 09:54:53 +0800

    Add description of Tencent Aurora 3Pro (s905x3-b)

--- build-armbian/common-files/rootfs/etc/custom_service/start_service.sh
+++ build-armbian/common-files/rootfs/etc/custom_service/start_service.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#========================================================================================
+#
+# This file is licensed under the terms of the GNU General Public
+# License version 2. This program is licensed "as is" without any
+# warranty of any kind, whether express or implied.
+#
+# This file is a part of the Armbian for Amlogic TV Boxes
+# https://github.com/ophub/amlogic-s9xxx-armbian
+#
+# Function: Customize the startup script, adding content as needed
+# Dependent script: /etc/rc.local
+# File path: /etc/custom_service/start_service.sh
+#
+#========================================================================================
 
-# Customize the startup script, adding content as needed
+# Add custom enabled alias extension load module.
+# For Tencent Aurora 3Pro (s905x3-b) box [ /etc/modprobe.d/blacklist.conf : blacklist btmtksdio ]
+modprobe btmtksdio 2>/dev/null
+
+# Add custom log
 echo "[$(date +"%Y.%m.%d.%H%M")] Hello World..." >/tmp/ophub_start_service.log
