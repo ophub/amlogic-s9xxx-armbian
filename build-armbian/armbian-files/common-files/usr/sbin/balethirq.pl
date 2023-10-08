6d25611951255f966ce2c9772cbc211bf47969ae
ophub <68696949+ophub@users.noreply.github.com>
2023-10-08 03:48:26 +0000

    Remove optimization for HinLink series devices

--- build-armbian/armbian-files/common-files/usr/sbin/balethirq.pl
+++ build-armbian/armbian-files/common-files/usr/sbin/balethirq.pl
@@ -221,30 +221,14 @@ sub board_special_config() {
         &optimize_eth_parameters("eth0","eth1");
     } elsif($board eq "FastRhino R68S") {
         &optimize_eth_parameters("eth0","eth1","eth2","eth3");
-    } elsif($board eq "HINLINK OPC-H66K Board") {
-        &optimize_eth_parameters("eth0","eth1","eth2");
-    } elsif($board eq "HINLINK OPC-H68K Board") {
-        &optimize_eth_parameters("eth0","eth1","eth2","eth3");
-    } elsif($board eq "HINLINK OPC-H69K Board") {
-        &optimize_eth_parameters("eth0","eth1","eth2","eth3");
     } elsif($board eq "Radxa E25") {
         &optimize_eth_parameters("eth0","eth1");
     } elsif($board eq "FriendlyElec NanoPi R5") {
         &optimize_eth_parameters("eth0","eth1","eth2");
     } elsif($board eq "Radxa ROCK 5B") {
         &optimize_eth_parameters("eth0","eth1");
-    } elsif($board eq "HINLINK OWL H88K Board") {
-        &optimize_eth_parameters("eth0","eth1");
     } elsif($board eq "Beelink IPC-R Board") {
         &optimize_eth_parameters("eth0","eth1","eth2","eth3","eth4","eth5");
-    } elsif($board eq "Hlink H28K") {
-        &optimize_eth_parameters("eth1");
-    } elsif($board eq "Hlink H66K") {
-        &optimize_eth_parameters("eth0","eth1","eth2");
-    } elsif($board eq "Hlink H68K") {
-        &optimize_eth_parameters("eth0","eth1","eth2","eth3");
-    } elsif($board eq "Hlink H69K") {
-        &optimize_eth_parameters("eth0","eth1","eth2");
     }
 }
 
