f55fc3b006ae828b9c2d304238079be398f9ce20
ophub <68696949+ophub@users.noreply.github.com>
2024-07-08 17:26:57 +0800

    Update pwm-fan.pl

--- build-armbian/armbian-files/different-files/chainedbox/rootfs/usr/bin/pwm-fan.pl
+++ build-armbian/armbian-files/different-files/chainedbox/rootfs/usr/bin/pwm-fan.pl
@@ -12,6 +12,9 @@ use File::Glob qw(bsd_glob);
 #慢抽走我家云内热风就行,没有必要强制降低cpu温度,当cpu突破75度,风扇全速运行,强制散热.
 #由于cpu重负载情况不多,所以风扇很少全速开启.
 #这套逻辑使用下来非常满意;
+#20240708 修复硬盘未待机低温切换未生效bug;
+#         调整未待机时的风扇启动停温度;
+#         修改部分无需变动的变量为未常量;
 #20240624 增加没有内置机械硬盘的情况,可供设置,详细请看my @disks 相关几行;
 #20240618 由于无法在不影响硬盘进入待机状态前提下读取硬盘温度,所以只能简单判断硬盘工作状态,调整风扇启停温度;
 #         增加硬盘待机状态检测,如果硬盘待机,风扇可在cpu温度较高时停转(50度),较高温度起转(65度);如果硬盘未待机,风扇可在cpu温度较低时停转(38度),较低温度起转(55度);
@@ -26,30 +29,38 @@ use File::Glob qw(bsd_glob);
 
 # 获取所有硬盘设备名称
 my @disks = bsd_glob("/dev/sd[a-z]");#如果你使用的硬盘比较多,由于我无法判断那块硬盘是内置硬盘,所以视所有硬盘为内置硬盘;两块以上硬盘无法判断内置硬盘带来问题是,风扇无法在cpu较高温度停转;
-   #@disks = ("/dev/sda");  #如果你能确定内置硬盘设备名,请去掉本行代码前面的井号,并修改为硬盘设备名  /dev/sda  或  /dev/sdb  或  /dev/sdc   ...
-   #@disks = ("0");	#如果你没有内置机械硬盘请去掉前面的#号,风扇会比较安静;
-														
+  #@disks = ("/dev/sdb");  #如果你能确定内置硬盘设备名,请去掉本行代码前面的井号,并修改为硬盘设备名  /dev/sda  或  /dev/sdb  或  /dev/sdc   ...
+  #@disks = ("0");	#如果你没有内置机械硬盘请去掉前面的#号,风扇会比较安静;
+
 # 速度最小值(满速是99),如果太小可能进入死区,风扇不转,需配合下面提示调整
 my $speed_min = 8;
 
 # 速度最大值(满速是99)
 my $speed_max = 99;
 
+#当硬盘待机时下面赋值生效
+#$temp_low = temp_low_high;
+#$temp_fanOn = temp_fanOn_high;
+#当硬盘没有待机时下面赋值生效
+#$temp_low = temp_low_low;
+#$temp_fanOn = temp_fanOn_low;
+
+
 # 温度低限(摄氏度): 小于等于此温度风扇停转
 my $temp_low = 38;
 
 # 温度低限(摄氏度):根据硬盘待机与否设置停转温度
-my $temp_low_high = 50;#硬盘待机时cpu温度小于50度,风扇停转
+use constant temp_low_high => 50;#硬盘待机时cpu温度小于50度,风扇停转
 
-my $temp_low_low = 38;#硬盘非待机时cpu温度小于38度,风扇停转
+use constant temp_low_low => 38;#硬盘非待机时cpu温度小于38度,风扇停转
 
 # 风扇启动温度低限(摄氏度): 大于于等于此值按启动风扇散热
-my $temp_fanOn = 55;
+my $temp_fanOn = 50;
 
 # 风扇启动温度低限(摄氏度):根据硬盘待机与否设置停转温度
-my $temp_fanOn_high = 65;#硬盘待机时cpu温度大于65度,风扇启动
+use constant temp_fanOn_high => 65;#硬盘待机时cpu温度大于65度,风扇启动
 
-my $temp_fanOn_low = 55;#硬盘非待机时cpu温度大于55度,风扇启动
+use constant temp_fanOn_low => 50;#硬盘非待机时cpu温度大于55度,风扇启动
 
 # 风扇启动占空比参数(%): 风扇启动时使用固定占空比：风扇启动占空比参数,设置要点:保证能启动同时风扇噪音不要过大
 my $duty_cycle_on = 5;
@@ -85,6 +96,7 @@ if( ($fixed_speed ne "") &&
 } else { 
     # 否则自动调速
     while(1) {
+		#print "ok 1.\n";
         &auto_speed;
         sleep($interval);    
     }
@@ -153,8 +165,8 @@ sub set_fixed_speed {
 
 sub auto_speed {
 
-  my $non_standby_disks = 0;
-    if($disks[0] != "0"){
+    my $non_standby_disks = 0;
+    if($disks[0] !~ m#0#){
       foreach my $disk (@disks) {
       # 检查每个硬盘的电源状态
       my $state = `hdparm -C $disk`;
@@ -166,28 +178,28 @@ sub auto_speed {
     }
      #根据硬盘状态调整风扇启停温度
      if($non_standby_disks == 0){
-        $temp_low = $temp_low_high;
-        $temp_fanOn = $temp_fanOn_high;
+        $temp_low = temp_low_high;
+        $temp_fanOn = temp_fanOn_high;
      }else {
-        $temp_low = $temp_low_low;
-        $temp_fanOn = $temp_fanOn_low;
+        $temp_low = temp_low_low;
+        $temp_fanOn = temp_fanOn_low;
      }
 
     my $temp = &get_soc_temp;
     #my $coeff_temp;
     if($temp <= $temp_low) {
-	$coeff_temp = 0;
-	$fanclose_temp = 0;
-	#$fanclose_temp = 1; #去掉此行代码前的#测试风扇是否可以维持旋转,测试完毕此行代码前加#
+	        $coeff_temp = 0;
+	        $fanclose_temp = 0;
+	        #$fanclose_temp = 1; #去掉此行代码前的#测试风扇是否可以维持旋转,测试完毕此行代码前加#
         } elsif($temp > $temp_high) {
                 $coeff_temp = 1;
-	              $fanclose_temp = 1;
-                } elsif(($temp > $temp_fanOn)&&($fanclose_temp == 0)){
-	                       $fan_up = 1; #风扇启转,设置首启转志位
-	                       $fanclose_temp = 1;
-	                       }else {
-                            $coeff_temp = ($temp - $temp_low) / (($temp_high - $temp_low)*$fan_n);
-                            }
+	            $fanclose_temp = 1;
+            } elsif(($temp > $temp_fanOn)&&($fanclose_temp == 0)){
+	                $fan_up = 1; #风扇启转,设置首启转志位
+	                $fanclose_temp = 1; 
+	            }else {
+                        $coeff_temp = ($temp - $temp_low) / (($temp_high - $temp_low)*$fan_n);
+                    }
 
     my $coeff_speed_min = $speed_min / $speed_max  * ($speed_max / 100);
     my $coeff_speed = $coeff_temp; 
@@ -196,13 +208,13 @@ sub auto_speed {
     my $duty_cycle = int($coeff_speed * $period);
 
     if($fanclose_temp == 0) {
-	$duty_cycle = 0;
+	        $duty_cycle = 0;
         }elsif(($fanclose_temp == 1)&&($fan_up == 1)){
-		$duty_cycle = $duty_cycle_on ;
-		$fan_up = 0; #风扇已启动,清零首启转标志位
+		    $duty_cycle = $duty_cycle_on ;
+		    $fan_up = 0; #风扇已启动,清零首启转标志位
 		}else{
-		}
-    
+		 }
+
     open my $fh, ">", "/sys/class/pwm/pwmchip0/pwm0/duty_cycle";
     print $fh "$duty_cycle\n";
     close $fh;
