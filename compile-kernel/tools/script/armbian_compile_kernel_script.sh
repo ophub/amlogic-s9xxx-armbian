733b3250aaaa158448316969ce16dcaaad46a8c9
ophub <68696949+ophub@users.noreply.github.com>
2025-02-12 14:27:54 +0800

    Changing the default kernel options

--- compile-kernel/tools/script/armbian_compile_kernel_script.sh
+++ compile-kernel/tools/script/armbian_compile_kernel_script.sh
@@ -65,7 +65,7 @@ ophub_release_file="/etc/ophub-release"
 # Set the default for downloading kernel sources from github.com
 repo_owner="unifreq"
 repo_branch="main"
-build_kernel=("6.1.y" "6.6.y")
+build_kernel=("6.1.y" "6.12.y")
 # Set whether to use the latest kernel, options: [ true / false ]
 auto_kernel="true"
 # Set whether to apply custom kernel patches, options: [ true / false ]
