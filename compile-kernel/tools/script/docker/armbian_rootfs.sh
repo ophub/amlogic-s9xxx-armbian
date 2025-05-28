f6bbc4f9631eddde03ddc2c8cfba00780781ce38
ophub <68696949+ophub@users.noreply.github.com>
2025-05-28 14:59:51 +0800

    Add script to create docker image

--- compile-kernel/tools/script/docker/armbian_rootfs.sh
+++ compile-kernel/tools/script/docker/armbian_rootfs.sh
@@ -11,8 +11,8 @@
 # Description: Redo the Armbian rootfs file.
 # Copyright (C) 2021- https://github.com/ophub/amlogic-s9xxx-armbian
 #
-# Command: ./compile-kernel/tools/script/redo_armbian_rootfs.sh -v <VERSION_CODENAME>
-#          ./compile-kernel/tools/script/redo_armbian_rootfs.sh -v bookworm
+# Command: ./compile-kernel/tools/script/docker/armbian_rootfs.sh -v <VERSION_CODENAME>
+#          ./compile-kernel/tools/script/docker/armbian_rootfs.sh -v bookworm
 #
 #===================== Set make environment variables =====================
 #
@@ -22,6 +22,7 @@ build_path="${current_path}/build"
 image_path="${build_path}/output/images"
 cache_path="${build_path}/cache/rootfs"
 tmp_rootfs="${image_path}/tmp_rootfs"
+
 #
 # Set font color
 STEPS="[\033[95m STEPS \033[0m]"
@@ -41,23 +42,30 @@ init_var() {
     echo -e "${STEPS} Start Initializing Variables..."
 
     # If it is followed by [ : ], it means that the option requires a parameter value
-    get_all_ver="$(getopt "v:" "${@}")"
+    local options="v:"
+    parsed_args=$(getopt -o "${options}" -- "${@}")
+    [[ ${?} -ne 0 ]] && error_msg "Parameter parsing failed."
+    eval set -- "${parsed_args}"
 
-    while [[ -n "${1}" ]]; do
+    while true; do
         case "${1}" in
         -v | --VERSION_CODENAME)
             if [[ -n "${2}" ]]; then
                 version_codename="${2}"
-                shift
+                shift 2
             else
                 error_msg "Invalid -v parameter [ ${2} ]!"
             fi
             ;;
+        --)
+            shift
+            break
+            ;;
         *)
-            error_msg "Invalid option [ ${1} ]!"
+            [[ -n "${1}" ]] && error_msg "Invalid option [ ${1} ]!"
+            break
             ;;
         esac
-        shift
     done
 
     echo -e "${INFO} VERSION CODENAME: [ ${version_codename} ]"
@@ -82,27 +90,32 @@ redo_rootfs() {
     [[ "${?}" == "0" ]] && echo -e "${INFO} 01. Temporary directory creation completed." || error_msg "01. Failed to create directory!"
 
     # Redo Armbian rootfs
-    [[ -n "${rootfs_file}" ]] && {
+    if [[ -n "${rootfs_file}" ]]; then
         sudo tar -mxf ${rootfs_file} -C ${tmp_rootfs}/
+
         cd ${tmp_rootfs}/
         sudo tar -czf ${rootfs_save_name}.tar.gz *
         sudo mv -f ${rootfs_save_name}.tar.gz ../
         [[ "${?}" == "0" ]] && echo -e "${INFO} 02. Making Armbian rootfs completed." || error_msg "02. Failed to redo rootfs!"
-    } || error_msg "02. Failed to find rootfs file!"
+    else
+        error_msg "02. Failed to find rootfs file!"
+    fi
 
     # Rename Armbian image
-    [[ -n "${image_file}" ]] && {
+    if [[ -n "${image_file}" ]]; then
         cd ${image_path}/
         mv -f ${image_file} ${image_save_name}
         pigz -qf *.img || gzip -qf *.img
         [[ "${?}" == "0" ]] && echo -e "${INFO} 03. Renaming Armbian image completed." || error_msg "03. Failed to rename the image!"
-    } || error_msg "03. Failed to find Armbian image!"
+    else
+        error_msg "03. Failed to find Armbian image!"
+    fi
 
     # Add sha256sum verification files
     cd ${image_path}/
     sudo rm -rf $(ls . | grep -vE ".img.gz|.tar.gz" | xargs) 2>/dev/null
     for file in *; do [[ ! -d "${file}" ]] && sha256sum "${file}" >"${file}.sha"; done
-    [[ "${?}" == "0" ]] && echo -e "${INFO} 04. The files in the current directory:\n$(ls -l .)" || error_msg "04. Failed to add sha256sum!"
+    [[ "${?}" == "0" ]] && echo -e "${INFO} 04. The files in the current directory:\n$(ls -lh .)" || error_msg "04. Failed to add sha256sum!"
 
     # Delete Armbian build source codes and temporary files
     cd ${build_path}/
