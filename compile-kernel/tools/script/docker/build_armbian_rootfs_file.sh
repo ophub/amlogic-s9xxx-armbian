89bb13e6813fc3272fb87df5292b87d4d717f303
ophub <68696949+ophub@users.noreply.github.com>
2025-05-30 15:43:40 +0800

    Set terminal colors

--- compile-kernel/tools/script/docker/build_armbian_rootfs_file.sh
+++ compile-kernel/tools/script/docker/build_armbian_rootfs_file.sh
@@ -113,10 +113,42 @@ redo_rootfs() {
             echo -e "${INFO} 04. Adjusting the default account completed."
         } || error_msg "04. Failed to adjust root password!"
 
+        # Set terminal colors
+        [[ -f "usr/bin/dircolors" ]] && {
+            # Set the default color for command
+            sudo chmod +x usr/bin/dircolors
+
+            # Add color support for command and set the default prompt
+            sudo tee -a root/.bashrc > /dev/null <<'EOF'
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# Color prompt (green username@hostname + blue path)
+PS1='\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
+
+EOF
+            # Add the bashrc file to the root profile
+            sudo tee -a root/.profile > /dev/null <<'EOF'
+
+if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+fi
+
+EOF
+            echo -e "${INFO} 05. Adjusting the default color completed."
+        } || error_msg "05. Failed to adjust dircolors!"
+
         # Compress the rootfs file
         sudo tar -czf ${rootfs_save_name}.tar.gz *
         sudo mv -f ${rootfs_save_name}.tar.gz ../
-        [[ "${?}" == "0" ]] && echo -e "${INFO} 05. Making Armbian rootfs completed." || error_msg "05. Failed to redo rootfs!"
+        [[ "${?}" == "0" ]] && echo -e "${INFO} 06. Making Armbian rootfs completed." || error_msg "06. Failed to redo rootfs!"
     else
         error_msg "02. Failed to find rootfs file!"
     fi
@@ -126,21 +158,21 @@ redo_rootfs() {
         cd ${image_path}/
         mv -f ${image_file} ${image_save_name}
         pigz -qf *.img || gzip -qf *.img
-        [[ "${?}" == "0" ]] && echo -e "${INFO} 06. Renaming Armbian image completed." || error_msg "06. Failed to rename the image!"
+        [[ "${?}" == "0" ]] && echo -e "${INFO} 07. Renaming Armbian image completed." || error_msg "07. Failed to rename the image!"
     else
-        error_msg "06. Failed to find Armbian image!"
+        error_msg "07. Failed to find Armbian image!"
     fi
 
     # Add sha256sum verification files
     cd ${image_path}/
     sudo rm -rf $(ls . | grep -vE ".img.gz|.tar.gz" | xargs) 2>/dev/null
     for file in *; do [[ ! -d "${file}" ]] && sha256sum "${file}" >"${file}.sha"; done
-    [[ "${?}" == "0" ]] && echo -e "${INFO} 07. The files in the current directory:\n$(ls -lh .)" || error_msg "07. Failed to add sha256sum!"
+    [[ "${?}" == "0" ]] && echo -e "${INFO} 08. The files in the current directory:\n$(ls -lh .)" || error_msg "08. Failed to add sha256sum!"
 
     # Delete Armbian build source codes and temporary files
     cd ${build_path}/
     sudo rm -rf $(ls . | grep -v "^output$" | xargs)
-    [[ "${?}" == "0" ]] && echo -e "${INFO} 08. Armbian source code cleanup completed." || error_msg "08. Failed to clean up!"
+    [[ "${?}" == "0" ]] && echo -e "${INFO} 09. Armbian source code cleanup completed." || error_msg "09. Failed to clean up!"
 
     sync && sleep 3
 }
