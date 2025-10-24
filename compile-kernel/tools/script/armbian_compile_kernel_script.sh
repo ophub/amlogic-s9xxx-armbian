153058f18969724eab2dbe295f47878e72f771d9
ophub <68696949+ophub@users.noreply.github.com>
2025-10-24 18:14:44 +0800

    Set the default kernel compile toolchain to Clang

--- compile-kernel/tools/script/armbian_compile_kernel_script.sh
+++ compile-kernel/tools/script/armbian_compile_kernel_script.sh
@@ -89,7 +89,7 @@ gun_file="arm-gnu-toolchain-14.3.rel1-aarch64-aarch64-none-linux-gnu.tar.xz"
 # Set the toolchain path
 toolchain_path="/usr/local/toolchain"
 # Set the default cross-compilation toolchain: [ clang / gcc / gcc-14.2, etc. ]
-toolchain_name="gcc"
+toolchain_name="clang"
 
 # Set font color
 STEPS="[\033[95m STEPS \033[0m]"
